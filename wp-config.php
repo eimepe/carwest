e2401c5834df0b40091f0c6f7b047ba599189e0d
eimepe <eimepe@yahoo.com>
2018-04-03 02:27:14 -0500

    update database

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'carwest_wp1');
+define('DB_NAME', 'carwest');
 
 /** MySQL database username */
-define('DB_USER', 'carwest_wp1');
+define('DB_USER', 'eimepe');
 
 /** MySQL database password */
-define('DB_PASSWORD', '92040166809');
+define('DB_PASSWORD', 'eimepe73');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql-carwest.cdbsdrwiat5y.us-west-2.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
